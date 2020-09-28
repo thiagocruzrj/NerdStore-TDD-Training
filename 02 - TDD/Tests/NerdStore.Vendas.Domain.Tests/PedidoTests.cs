47c89f2ee12476b14e52990fa297192afa028aa2
Thiago <thagocruz@gmail.com>
2020-09-28 18:27:21 -0300

    Add necessary test to add new order

---
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid, "ProdutoTeste", 5, 100);
+
+            // Act
+            pedido.AdicionarItem(pedidoItem);
+
+            // Assert
+            Assert.Equal(500, pedido.ValorTotal);
+        }
+    }
+}
