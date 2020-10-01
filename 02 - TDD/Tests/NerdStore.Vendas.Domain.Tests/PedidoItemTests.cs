b26b7cc991b2ed6eb6bb9c90e2dec003f3afba5b
Thiago <thagocruz@gmail.com>
2020-10-01 10:54:34 -0300

    Refact pedido item tests

--- 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -10,26 +10,16 @@ namespace NerdStore.Vendas.Domain.Tests
         [Trait("Categoria", "Pedido Item Tests")]
         public void AdicionarItemPedido_ItemAcimaDoPermitido_DeveRetornarException()
         {
-            // Arrange
-            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
-            var pedidoId = Guid.NewGuid();
-            var pedidoItem = new PedidoItem(pedidoId, "ProdutoTeste", Pedido.MAX_UNIDADES_ITEM + 1, 100);
-
-            // Act & Assert
-            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
+            // Arrange Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "ProdutoTeste", Pedido.MAX_UNIDADES_ITEM + 1, 100));
         }
 
         [Fact(DisplayName = "Adicionar Item Pedido abaixo do permitido")]
         [Trait("Categoria", "Pedido Item Tests")]
         public void AdicionarItemPedido_ItemAbaixoDoPermitido_DeveRetornarException()
         {
-            // Arrange
-            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
-            var pedidoId = Guid.NewGuid();
-            var pedidoItem = new PedidoItem(pedidoId, "ProdutoTeste", Pedido.MIN_UNIDADES_ITEM - 1, 100);
-
-            // Act & Assert
-            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
+            // Arrange, Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "ProdutoTeste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
         }
     }
 }
