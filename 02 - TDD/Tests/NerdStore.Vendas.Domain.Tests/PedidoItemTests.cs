08cb4422f1fba5937d237aab6a2853e1e30688e1
Thiago <thagocruz@gmail.com>
2020-10-01 10:43:27 -0300

    Create PedidoItemTests.cs

---
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -0,0 +1,35 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoItemTests
+    {
+        [Fact(DisplayName = "Adicionar Item Pedido acima do permitido")]
+        [Trait("Categoria", "Pedido Item Tests")]
+        public void AdicionarItemPedido_ItemAcimaDoPermitido_DeveRetornarException()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(pedidoId, "ProdutoTeste", Pedido.MAX_UNIDADES_ITEM + 1, 100);
+
+            // Act & Assert
+            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
+        }
+
+        [Fact(DisplayName = "Adicionar Item Pedido abaixo do permitido")]
+        [Trait("Categoria", "Pedido Item Tests")]
+        public void AdicionarItemPedido_ItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(pedidoId, "ProdutoTeste", Pedido.MIN_UNIDADES_ITEM - 1, 100);
+
+            // Act & Assert
+            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
+        }
+    }
+}
