3243aeb6499740abc227410cf3f20c8d8bf86981
Thiago <thagocruz@gmail.com>
2020-10-01 10:56:18 -0300

    Update PedidoItemTests.cs

--- 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -6,7 +6,7 @@ namespace NerdStore.Vendas.Domain.Tests
 {
     public class PedidoItemTests
     {
-        [Fact(DisplayName = "Adicionar Item Pedido acima do permitido")]
+        [Fact(DisplayName = "Novo Item Pedido com unidades acima do permitido")]
         [Trait("Categoria", "Pedido Item Tests")]
         public void AdicionarItemPedido_ItemAcimaDoPermitido_DeveRetornarException()
         {
@@ -14,7 +14,7 @@ namespace NerdStore.Vendas.Domain.Tests
             Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "ProdutoTeste", Pedido.MAX_UNIDADES_ITEM + 1, 100));
         }
 
-        [Fact(DisplayName = "Adicionar Item Pedido abaixo do permitido")]
+        [Fact(DisplayName = "Novo Item Pedido com unidades abaixo do permitido")]
         [Trait("Categoria", "Pedido Item Tests")]
         public void AdicionarItemPedido_ItemAbaixoDoPermitido_DeveRetornarException()
         {
