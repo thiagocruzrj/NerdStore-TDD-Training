07a9804d7b431427c58cdda7057ed3394cd64e17
Thiago <thagocruz@gmail.com>
2020-10-02 19:29:23 -0300

    Create PedidoCommandHandlerTests.cs

---
+++ 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class PedidoCommandHandlerTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo Pedido com Sucesso")]
+        [Trait("Categoria", "Vendas - Pedido Commands Handler")]
+        public void AdicionarItem_NovoPedido_DeveExecutarComSucesso()
+        {
+        }
+    }
+}
