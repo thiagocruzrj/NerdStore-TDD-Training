d5e583b9fe9ce231918a9dad59e38cad59d0c5b6
Thiago <thagocruz@gmail.com>
2020-10-02 13:01:31 -0300

    Add red

--- 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -1,14 +1,31 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace NerdStore.Vendas.Domain.Tests
 {
     public class VoucherTests
     {
-        [Fact(DisplayName = "Validar Voucher Valor Valido")]
+        [Fact(DisplayName = "Validar Voucher Tipo Valor Valido")]
         [Trait("Categoria", "Vendas - Voucher")]
-        public void Voucher_ValidarValorVoucher_DeveEstarValido()
+        public void Voucher_ValidarTipoValorVoucher_DeveEstarValido()
         {
+            // Arrange
+            var voucher = new Voucher
+            {
+                Codigo = "PROMO-15-REAIS",
+                ValorDesconto = 15,
+                PercentualDesconto = null,
+                Quantidade = 1,
+                DataValidade = DateTime.Now,
+                Ativo = true,
+                Utilizado = false
+            };
 
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.True(result);
         }
     }
 }
