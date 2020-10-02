403030fdd3c3e6c1c5d1a5779c8c9be4fa1402a3
Thiago <thagocruz@gmail.com>
2020-10-02 12:52:52 -0300

    Create VoucherTests.cs

---
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validar Voucher Valor Valido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarValorVoucher_DeveEstarValido()
+        {
+
+        }
+    }
+}
