24e8e1e2f2892c6c1e3f97e1e5f15951ecfa4cd7
Thiago <thagocruz@gmail.com>
2020-10-02 18:52:08 -0300

    Add AdicionarItemPedidoCommand_CommandoEstaInalido_NaoDevePassarNaValidacao test

--- 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -19,5 +19,19 @@ namespace NerdStore.Vendas.Application.Tests.Pedidos
             // Assert
             Assert.True(result);
         }
+
+        [Fact(DisplayName = "Adicionar Item Command Inalido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_CommandoEstaInalido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var command = new AdicionarItemPedidoCommand(Guid.Empty, Guid.Empty, "", 0, 0);
+
+            // Act
+            var result = command.EhValido();
+
+            // Assert
+            Assert.False(result);
+        }
     }
 }
