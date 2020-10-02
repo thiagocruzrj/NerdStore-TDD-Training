fc91c44e457a675eaae63f30e9ec56cc3932dab8
Thiago <thagocruz@gmail.com>
2020-10-02 12:57:44 -0300

    Create Voucher.cs

---
+++ 02 - TDD/NerdStore.Vendas.Domain/Voucher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Voucher
+    {
+        public string Codigo { get; set; }
+        public decimal? PercentualDesconto { get; set; }
+        public decimal? ValorDesconto { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataValidade { get; set; }
+        public bool Ativo { get; set; }
+        public bool Utilizado { get; set; }
+    }
+}
