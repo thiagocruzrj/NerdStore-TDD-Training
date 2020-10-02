ed26550117b155fa7bf2a5121673d3782a146b12
Thiago <thagocruz@gmail.com>
2020-10-02 19:53:39 -0300

    Add IRequest to use the handler message

--- 02 - TDD/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -2,10 +2,11 @@
 using NerdStore.Vendas.Application.Events;
 using NerdStore.Vendas.Domain;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Application.Commands
 {
-    public class PedidoCommandHandler
+    public class PedidoCommandHandler : IRequestHandler<AdicionarItemPedidoCommand, bool>
     {
         private readonly IPedidoRepository _repository;
         private readonly IMediator _mediator;
@@ -16,10 +17,11 @@ namespace NerdStore.Vendas.Application.Commands
             _mediator = mediator;
         }
 
-        public bool Handle(AdicionarItemPedidoCommand message)
+        public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
         {
             _repository.Adicionar(Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId));
-            _mediator.Publish(new PedidoItemAdicionadoEvent());
+
+            await _mediator.Publish(new PedidoItemAdicionadoEvent(), cancellationToken);
             return true;
         }
     }
