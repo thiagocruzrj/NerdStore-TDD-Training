3792bb667ad3e38e1213acd6d68f68bf18cd3a9d
Thiago <thagocruz@gmail.com>
2020-10-02 18:35:16 -0300

    Add properties on AdicionarItemPedidoCommand

--- 02 - TDD/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
@@ -1,6 +1,13 @@
-﻿namespace NerdStore.Vendas.Application.Commands
+﻿using System;
+
+namespace NerdStore.Vendas.Application.Commands
 {
     public class AdicionarItemPedidoCommand
     {
+        public Guid ClienteId { get; set; }
+        public Guid ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
     }
 }
