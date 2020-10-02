87afc2180cafd03237401c6c219b5ebde79ff2c3
Thiago <thagocruz@gmail.com>
2020-10-02 17:52:04 -0300

    Create PedidoItem.cs

---
+++ 02 - TDD/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,34 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem
+    {
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            if (quantidade < Pedido.MIN_UNIDADES_ITEM)
+                throw new DomainException($"Limite minimo de {Pedido.MIN_UNIDADES_ITEM} itens excedido.");
+
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return ValorUnitario * Quantidade;
+        }
+    }
+}
