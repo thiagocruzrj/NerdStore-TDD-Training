7e22f3713643670715cf726f47aa602c69405c31
Thiago <thagocruz@gmail.com>
2020-10-02 19:57:26 -0300

    Add events properties to return

--- 02 - TDD/NerdStore.Vendas.Application/Events/PedidoItemAdicionadoEvent.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Events/PedidoItemAdicionadoEvent.cs
@@ -1,8 +1,26 @@
 ﻿using MediatR;
+using NerdStore.Core.Messages;
+using System;
 
 namespace NerdStore.Vendas.Application.Events
 {
-    public class PedidoItemAdicionadoEvent : INotification
+    public class PedidoItemAdicionadoEvent : Event
     {
+        public PedidoItemAdicionadoEvent(Guid clienteId, Guid pedidoId, Guid produtoId, string produtNome, decimal valorUnitario, int quantidade)
+        {
+            ClienteId = clienteId;
+            PedidoId = pedidoId;
+            ProdutoId = produtoId;
+            ProdutNome = produtNome;
+            ValorUnitario = valorUnitario;
+            Quantidade = quantidade;
+        }
+
+        public Guid ClienteId { get; set; }
+        public Guid PedidoId { get; set; }
+        public Guid ProdutoId { get; set; }
+        public string ProdutNome { get; set; }
+        public decimal ValorUnitario { get; set; }
+        public int Quantidade { get; set; }
     }
 }
