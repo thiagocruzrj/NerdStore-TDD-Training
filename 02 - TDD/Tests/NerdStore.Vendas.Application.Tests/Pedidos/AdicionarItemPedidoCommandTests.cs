6873a14fe25f26266b3750a3e689687fd58521f5
Thiago <thagocruz@gmail.com>
2020-10-02 18:29:25 -0300

    Create AdicionarItemPedidoCommandTests.cs

---
+++ 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar Item Command Valido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_CommandoEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
