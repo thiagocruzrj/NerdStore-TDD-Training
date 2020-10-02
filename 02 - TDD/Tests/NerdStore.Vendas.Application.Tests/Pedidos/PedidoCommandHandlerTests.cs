b3727d92d19aa5d3cc70e30c0bbe3e962ad1bb97
Thiago <thagocruz@gmail.com>
2020-10-02 19:37:12 -0300

    Create red test no compile

--- 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Application.Tests/Pedidos/PedidoCommandHandlerTests.cs
@@ -1,4 +1,9 @@
-﻿using Xunit;
+﻿using Moq;
+using Moq.AutoMock;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Domain;
+using System;
+using Xunit;
 
 namespace NerdStore.Vendas.Application.Tests.Pedidos
 {
@@ -8,6 +13,19 @@ namespace NerdStore.Vendas.Application.Tests.Pedidos
         [Trait("Categoria", "Vendas - Pedido Commands Handler")]
         public void AdicionarItem_NovoPedido_DeveExecutarComSucesso()
         {
+            // Arrage
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(), Guid.NewGuid(), "ProdutoTeste", 2, 100);
+
+            var mocker = new AutoMocker();
+            var pedidoHandler = mocker.CreateInstance<PedidoCommandHandler>();
+
+            // Act
+            var result = pedidoHandler.Handle(pedidoCommand);
+
+            // Assert
+            Assert.True(result);
+            mocker.GetMock<IPedidoRepository>().Verify(r => r.Adicionar(It.IsAny<Pedido>()), Times.Once);
+            mocker.GetMock<IMediator>().Verify(r => r.Publish(It.IsAny<INotification>()), Times.Once);
         }
     }
 }
