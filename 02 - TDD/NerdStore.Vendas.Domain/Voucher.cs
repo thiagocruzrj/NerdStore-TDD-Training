c4ec97ff0ca91dbddc962fd49eb97526f7a86941
Thiago <thagocruz@gmail.com>
2020-10-02 13:20:10 -0300

    Add base entity voucher validation

--- 02 - TDD/NerdStore.Vendas.Domain/Voucher.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/Voucher.cs
@@ -40,7 +40,53 @@ namespace NerdStore.Vendas.Domain
         public static string UtilizadoErroMsg => "Esse voucher já foi utilizado";
         public static string QuantidadeErroMsg => "Voucher não está mais disponivel";
         public static string ValorDescontoErroMsg => "O valor do desconto precisa ser superior a 0";
-        public static string PercentualErroMsg => "O valor da porcentagem precisa ser superior a 0";
+        public static string PercentualDescontoErroMsg => "O valor da porcentagem precisa ser superior a 0";
+
+        public VoucherAplicavelValidation()
+        {
+            RuleFor(c => c.Codigo)
+                .NotEmpty()
+                .WithMessage(CodigoErroMsg);
+
+            RuleFor(c => c.DataValidade)
+                .Must(DataVencimentoSuperiorAtual)
+                .WithMessage(DataValidadeErrorMsg);
+
+            RuleFor(c => c.Ativo)
+                .Equal(true)
+                .WithMessage(AtivoErroMsg);
+
+            RuleFor(c => c.Utilizado)
+                .Equal(false)
+                .WithMessage(UtilizadoErroMsg);
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage(QuantidadeErroMsg);
+
+            When(c => c.TipoDescontoVoucher == TipoDescontoVoucher.Porcentagem, () =>
+            {
+                RuleFor(f => f.PercentualDesconto)
+                    .NotNull()
+                    .WithMessage(PercentualDescontoErroMsg)
+                    .GreaterThan(0)
+                    .WithMessage(PercentualDescontoErroMsg);
+            });
+
+            When(c => c.TipoDescontoVoucher == TipoDescontoVoucher.Valor, () =>
+            {
+                RuleFor(f => f.ValorDesconto)
+                    .NotNull()
+                    .WithMessage(ValorDescontoErroMsg)
+                    .GreaterThan(0)
+                    .WithMessage(ValorDescontoErroMsg);
+            });
+
+        }
+        protected static bool DataVencimentoSuperiorAtual(DateTime dataValidade)
+        {
+            return dataValidade >= DateTime.Now;
+        }
     }
 
     public enum TipoDescontoVoucher
