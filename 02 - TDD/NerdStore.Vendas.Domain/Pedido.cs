3d9cb4743b85bd1d50b9e62df3d82f08d1f114e4
Thiago <thagocruz@gmail.com>
2020-10-02 17:52:02 -0300

    Update Pedido.cs

--- 02 - TDD/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/Pedido.cs
@@ -154,42 +154,4 @@ namespace NerdStore.Vendas.Domain
             }
         }
     }
-
-    public enum PedidoStatus
-    {
-        Rascunho = 0,
-        Iniciado = 1,
-        Pago = 4,
-        Entregue = 5,
-        Cancelado = 6
-    }
-
-    public class PedidoItem
-    {
-        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
-        {
-            if (quantidade < Pedido.MIN_UNIDADES_ITEM)
-                throw new DomainException($"Limite minimo de {Pedido.MIN_UNIDADES_ITEM} itens excedido.");
-
-            ProdutoId = produtoId;
-            ProdutoNome = produtoNome;
-            Quantidade = quantidade;
-            ValorUnitario = valorUnitario;
-        }
-
-        public Guid ProdutoId { get; private set; }
-        public string ProdutoNome { get; private set; }
-        public int Quantidade { get; private set; }
-        public decimal ValorUnitario { get; private set; }
-
-        internal void AdicionarUnidades(int unidades)
-        {
-            Quantidade += unidades;
-        }
-
-        internal decimal CalcularValor()
-        {
-            return ValorUnitario * Quantidade;
-        }
-    }
 }
