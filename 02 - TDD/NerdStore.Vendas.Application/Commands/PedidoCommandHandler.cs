b848d5932b071cc54d28a2b6b6ca558b1568da7c
Thiago <thagocruz@gmail.com>
2020-10-02 19:42:42 -0300

    Create PedidoCommandHandler.cs

---
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace NerdStore.Vendas.Application.Commands
+{
+    public class PedidoCommandHandler
+    {
+        public bool Handle(AdicionarItemPedidoCommand adicionarItemPedido)
+        {
+            return true;
+        }
+    }
+}
