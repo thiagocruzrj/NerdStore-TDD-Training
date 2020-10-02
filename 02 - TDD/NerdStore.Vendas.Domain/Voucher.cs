1c55c64d2467ea7cd46f3984a3007ddd3f67152c
Thiago <thagocruz@gmail.com>
2020-10-02 13:05:15 -0300

    Add enum and change all properties set to privete

--- 02 - TDD/NerdStore.Vendas.Domain/Voucher.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/Voucher.cs
@@ -4,17 +4,24 @@ namespace NerdStore.Vendas.Domain
 {
     public class Voucher
     {
-        public string Codigo { get; set; }
-        public decimal? PercentualDesconto { get; set; }
-        public decimal? ValorDesconto { get; set; }
-        public int Quantidade { get; set; }
-        public DateTime DataValidade { get; set; }
-        public bool Ativo { get; set; }
-        public bool Utilizado { get; set; }
+        public string Codigo { get; private set; }
+        public decimal? PercentualDesconto { get; private set; }
+        public decimal? ValorDesconto { get; private set; }
+        public TipoDescontoVoucher TipoDescontoVoucher { get; private set; }
+        public int Quantidade { get; private set; }
+        public DateTime DataValidade { get; private set; }
+        public bool Ativo { get; private set; }
+        public bool Utilizado { get; private set; }
 
         public bool ValidarSeAplicavel()
         {
-            return false;
+            return true;
         }
+    }
+
+    public enum TipoDescontoVoucher
+    {
+        Porcentagem = 0,
+        Valor = 1
     }
 }
