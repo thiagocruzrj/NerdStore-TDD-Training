ce60fb8c88873d107687e7cbd57f41cf459cc4be
Thiago <thagocruz@gmail.com>
2020-10-02 18:39:22 -0300

    Add validations

--- 02 - TDD/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
@@ -34,4 +34,29 @@ namespace NerdStore.Vendas.Application.Commands
         public static string QtdMaxErroMsg => $"A quantidade máxima de um item é {Pedido.MAX_UNIDADES_ITEM}";
         public static string QtdMinErroMsg => "A quantidade miníma de um item é 1";
         public static string ValorErroMsg => "O valor do item precisa ser maior que 0";
+
+        public AdicionarItemPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage(IdClienteErroMsg);
+
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage(IdProdutoErroMsg);
+
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .WithMessage(NomeErroMsg);
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage(QtdMinErroMsg)
+                .LessThan(Pedido.MAX_UNIDADES_ITEM)
+                .WithMessage(QtdMaxErroMsg);
+
+            RuleFor(c => c.ValorUnitario)
+                .GreaterThan(0)
+                .WithMessage(ValorErroMsg);
+        }
 }
