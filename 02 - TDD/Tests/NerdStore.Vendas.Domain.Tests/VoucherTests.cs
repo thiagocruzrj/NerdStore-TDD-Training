3eb4db6546488543b55c24083810d71863c46854
Thiago <thagocruz@gmail.com>
2020-10-02 13:29:37 -0300

    Add invalid voucher test

--- 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -18,5 +18,19 @@ namespace NerdStore.Vendas.Domain.Tests
             // Assert
             Assert.True(result.IsValid);
         }
+
+        [Fact(DisplayName = "Validar Voucher Tipo Valor Invalido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarTipoValorVoucher_DeveEstarInvalido()
+        {
+            // Arrange
+            var voucher = new Voucher("PROMO-15-REAIS", null, 15, TipoDescontoVoucher.Valor, 1, DateTime.Now.AddDays(15), false, true);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
     }
 }
