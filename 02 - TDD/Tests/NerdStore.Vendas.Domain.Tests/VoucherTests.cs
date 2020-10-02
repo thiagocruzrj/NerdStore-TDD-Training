8f0d70a6c8bcf8560ae8eb3a72abffde395bb83f
Thiago <thagocruz@gmail.com>
2020-10-02 13:37:20 -0300

    Add porcent discount valid test

--- 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
+++ 02 - TDD/Tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -20,6 +20,20 @@ namespace NerdStore.Vendas.Domain.Tests
             Assert.True(result.IsValid);
         }
 
+        [Fact(DisplayName = "Validar Voucher Tipo Porcentagem Valido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarTipoPorcentagemVoucher_DeveEstarValido()
+        {
+            // Arrange
+            var voucher = new Voucher("PROMO-15-PORCENTO", 15, 0, TipoDescontoVoucher.Porcentagem, 1, DateTime.Now.AddDays(15), true, false);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
         [Fact(DisplayName = "Validar Voucher Tipo Valor Invalido")]
         [Trait("Categoria", "Vendas - Voucher")]
         public void Voucher_ValidarTipoValorVoucher_DeveEstarInvalido()
