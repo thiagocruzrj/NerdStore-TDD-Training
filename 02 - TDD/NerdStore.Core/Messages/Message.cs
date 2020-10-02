e285706e8d1ff251244e5a2d8a53cdb603102039
Thiago <thagocruz@gmail.com>
2020-10-02 18:47:36 -0300

    add Command class

--- 02 - TDD/NerdStore.Core/Messages/Message.cs
+++ 02 - TDD/NerdStore.Core/Messages/Message.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 
 namespace NerdStore.Core.Messages
 {
     public abstract class Message
     {
-        public string MessageType { get; private set; }
-        public Guid AggregateId { get; private set; }
+        public string MessageType { get; protected set; }
+        public Guid AggregateId { get; protected set; }
 
-        public Message()
+        protected Message()
         {
             MessageType = GetType().Name;
+        }
+    }
+
+    public abstract class Command : Message
+    {
+        public DateTime TimesStamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            TimesStamp = DateTime.Now;
         }
     }
 }
