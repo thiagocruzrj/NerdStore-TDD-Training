f0afc1eafe3177f51ebac3dcccdd1fe9eecf873e
Thiago <thagocruz@gmail.com>
2020-10-02 18:45:33 -0300

    Create Message.cs

---
+++ 02 - TDD/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Message
+    {
+        public string MessageType { get; private set; }
+        public Guid AggregateId { get; private set; }
+
+        public Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
