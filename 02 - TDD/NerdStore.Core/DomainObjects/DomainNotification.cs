07d5caad76868b829c0c2d2f32c4dd1e282e620b
Thiago <thagocruz@gmail.com>
2020-10-02 23:25:32 -0300

    Create DomainNotification.cs

---
+++ 02 - TDD/NerdStore.Core/DomainObjects/DomainNotification.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class DomainNotification: Message, INotification
+    {
+        public DomainNotification(DateTime timestamp, Guid domainNotificationId, string key, string value, int version)
+        {
+            Timestamp = DateTime.Now;
+            DomainNotificationId = Guid.NewGuid();
+            Key = key;
+            Value = value;
+            Version = 1;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public Guid DomainNotificationId { get; private set; }
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+        public int Version { get; private set; }
+    }
+}
