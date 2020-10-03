ac636e62a1f267e7ece3d3a44e6984bc137dc078
Thiago <thagocruz@gmail.com>
2020-10-02 21:52:39 -0300

    Create VendasContext.cs

---
+++ 02 - TDD/NerdStore.Vendas.Data/VendasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Core.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Data
+{
+    public class VendasContext : DbContext, IUnitOfWork
+    {
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
