d1a645a61e1f755dafc3d61b15fbbf0d3a2dc7ac
Thiago <thagocruz@gmail.com>
2020-10-02 22:34:54 -0300

    Add new methods

--- 02 - TDD/NerdStore.Vendas.Domain/IPedidoRepository.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/IPedidoRepository.cs
@@ -1,9 +1,14 @@
 ﻿using NerdStore.Core.Data;
+using System;
+using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Domain
 {
     public interface IPedidoRepository : IRepository<Pedido>
     {
         void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+        Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
+        void AdicionarItem(PedidoItem pedido);
     }
 }
