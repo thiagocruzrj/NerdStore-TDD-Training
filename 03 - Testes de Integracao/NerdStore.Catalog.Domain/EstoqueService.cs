eee38a677c39c30b720271d06546432a82346591
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 19:08:48 -0300

    Create EstoqueService.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/EstoqueService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        public Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
