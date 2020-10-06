52dfc80d219d1e66292cea08ad2b9a2b59563c90
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 20:45:23 -0300

    Create ProdutoMapping.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalog.Domain;
+
+namespace NerdStore.Catalog.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
