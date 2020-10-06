87c9eba3ab83f9a9594ee2c2b541d9d57ea39c46
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 20:43:49 -0300

    Create CategoriaMapping.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Data/Mappings/CategoriaMapping.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalog.Domain;
+
+namespace NerdStore.Catalog.Data.Mappings
+{
+    public class CategoriaMapping : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
