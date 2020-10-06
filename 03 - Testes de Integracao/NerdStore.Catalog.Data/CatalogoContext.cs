d6a43ffa2118ee3b5bb1fa622e97db827db5b062
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 20:04:49 -0300

    Create CatalogoContext.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Data/CatalogoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Core.Data;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalog.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public Task<bool> Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
