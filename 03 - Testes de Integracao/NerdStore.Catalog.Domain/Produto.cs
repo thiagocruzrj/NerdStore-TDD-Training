715a0007a284f37d0f1ba6844794e6520098c715
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 18:52:21 -0300

    Create Produto.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/Produto.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+        protected Produto() { }
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            Dimensoes = dimensoes;
+        }
+    }
+}
