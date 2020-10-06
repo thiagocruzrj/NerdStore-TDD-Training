9934b44042642189a2fa059b323c41adf4e6845c
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 19:22:46 -0300

    Implement DebitarItemEstoque method

--- 03 - Testes de Integracao/NerdStore.Catalog.Domain/EstoqueService.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/EstoqueService.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using NerdStore.Core.DomainObjects;
 using System;
 using System.Threading.Tasks;
 
@@ -17,8 +18,25 @@ namespace NerdStore.Catalog.Domain
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
-            if (!await DebitarEstoque(produtoId, quantidade)) return false;
+            if (!await DebitarItemEstoque(produtoId, quantidade)) return false;
             return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        private async Task<bool> DebitarItemEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            if(!produto.PossuiEstoque(quantidade))
+            {
+                await _mediator.Publish(new DomainNotification("Estoque", $"Produto - {produto.Nome} sem estoque"));
+                return false;
+            }
+
+            produto.DebitarEstoque(quantidade);
+            _produtoRepository.Atualizar(produto);
+            return true;
         }
 
         public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
