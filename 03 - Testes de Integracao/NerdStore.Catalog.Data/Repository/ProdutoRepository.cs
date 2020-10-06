df1398d37185c953ab2c1e3e23863d94e4f89028
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 20:31:31 -0300

    Create ProdutoRepository.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,58 @@
+﻿using NerdStore.Catalog.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalog.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        public IUnitOfWork UnitOfWork => throw new NotImplementedException();
+
+        public void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Adicionar(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Categoria>> ObterCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Produto> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Produto>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
