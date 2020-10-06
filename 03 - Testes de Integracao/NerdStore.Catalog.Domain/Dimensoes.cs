1517d83726310d2b7fc853c79cde4d8a4e8b8174
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 18:56:12 -0300

    Create Dimensoes.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/Dimensoes.cs
@@ -0,0 +1,16 @@
+﻿namespace NerdStore.Catalog.Domain
+{
+    public class Dimensoes
+    {
+        public decimal Altura { get; private set; }
+        public decimal Largura { get; private set; }
+        public decimal Profundidade { get; private set; }
+
+        public Dimensoes(decimal altura, decimal largura, decimal profundidade)
+        {
+            Altura = altura;
+            Largura = largura;
+            Profundidade = profundidade;
+        }
+    }
+}
