e19c8725f4de377267b1296880fd65811c998f44
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 18:40:46 -0300

    Create Categoria.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/Categoria.cs
@@ -0,0 +1,25 @@
+﻿using NerdStore.Core.DomainObjects;
+using System.Collections.Generic;
+
+namespace NerdStore.Catalog.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public string Codigo { get; private set; }
+        public ICollection<Produto> Produtos { get; set; }
+
+        public Categoria() { }
+
+        public Categoria(string nome, string codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
