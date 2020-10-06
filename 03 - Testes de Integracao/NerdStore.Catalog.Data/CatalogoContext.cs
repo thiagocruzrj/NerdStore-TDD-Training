6a3afe22576bcd73d32c76fb201ba50bc57e1003
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 20:29:44 -0300

    Add OnModelCreating method

--- 03 - Testes de Integracao/NerdStore.Catalog.Data/CatalogoContext.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Data/CatalogoContext.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalog.Domain;
 using NerdStore.Core.Data;
+using NerdStore.Core.Messages;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NerdStore.Catalog.Data
 {
     public class CatalogoContext : DbContext, IUnitOfWork
     {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(250)");
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
         public Task<bool> Commit()
         {
             throw new System.NotImplementedException();
