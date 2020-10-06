c73447e04e809c0d56f9b9e043597a3d6ff55fac
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 19:15:03 -0300

    add dependencies

--- 03 - Testes de Integracao/NerdStore.Catalog.Domain/EstoqueService.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Domain/EstoqueService.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Threading.Tasks;
 
 namespace NerdStore.Catalog.Domain
 {
     public class EstoqueService : IEstoqueService
     {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly Mediator _mediator;
+
+        public EstoqueService(IProdutoRepository produtoRepository, Mediator mediator)
+        {
+            _produtoRepository = produtoRepository;
+            _mediator = mediator;
+        }
+
         public Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
             throw new NotImplementedException();
