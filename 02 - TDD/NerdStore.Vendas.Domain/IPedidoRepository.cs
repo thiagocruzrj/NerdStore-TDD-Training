9d3387659526a10dcb9a6cf327d1f0924ca67f75
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 22:01:54 -0300

    Update IPedidoRepository.cs

--- 02 - TDD/NerdStore.Vendas.Domain/IPedidoRepository.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/IPedidoRepository.cs
@@ -1,15 +1,22 @@
 ﻿using NerdStore.Core.Data;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Domain
 {
     public interface IPedidoRepository : IRepository<Pedido>
     {
+        Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId);
+        Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
         void Adicionar(Pedido pedido);
         void Atualizar(Pedido pedido);
-        Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId);
-        void AdicionarItem(PedidoItem pedido);
-        void AtualizarItem(PedidoItem pedido);
+
+        Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId);
+        void AdicionarItem(PedidoItem pedidoItem);
+        void AtualizarItem(PedidoItem pedidoItem);
+        void RemoverItem(PedidoItem pedidoItem);
+
+        Task<Voucher> ObterVoucherPorCodigo(string codigo);
     }
 }
