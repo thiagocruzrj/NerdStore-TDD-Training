71b9ca9a9c0bc1d4f03ee01ceb3ec6adb80f86b6
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:05:43 -0300

    Create ProdutoAppService.cs

---
+++ 03 - Testes de Integracao/NerdStore.Catalog.Application/Services/ProdutoAppService.cs
@@ -0,0 +1,55 @@
+﻿using NerdStore.Catalog.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalog.Application.Services
+{
+    public class ProdutoAppService : IProdutoAppService
+    {
+        public Task AdicionarProduto(ProdutoViewModel produtoViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarProduto(ProdutoViewModel produtoViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoViewModel> DebitarEstoque(Guid id, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CategoriaViewModel>> ObterCategorias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
