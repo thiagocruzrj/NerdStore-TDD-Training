37ac1a67d388c67dcf2c38ba280fbbf85b621777
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 22:07:15 -0300

    Create PedidoQueries.cs

---
+++ 02 - TDD/NerdStore.Vendas.Application/Queries/PedidoQueries.cs
@@ -0,0 +1,20 @@
+﻿using NerdStore.Vendas.Application.Queries.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Queries
+{
+    public class PedidoQueries : IPedidoQueries
+    {
+        public Task<CarrinhoViewModel> ObterCarrinhoCliente(Guid clienteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PedidoViewModel>> ObterPedidosCliente(Guid clienteId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
