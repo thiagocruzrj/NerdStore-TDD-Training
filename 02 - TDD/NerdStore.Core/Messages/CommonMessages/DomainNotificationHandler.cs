723324bc28d4cd0e823cb501a8dd4d6ff5210148
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:49:15 -0300

    Create DomainNotificationHandler.cs

---
+++ 02 - TDD/NerdStore.Core/Messages/CommonMessages/DomainNotificationHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.Messages.CommonMessages
+{
+    public class DomainNotificationHandler : INotificationHandler<DomainNotification>
+    {
+        private List<DomainNotification> _notifications;
+
+        public DomainNotificationHandler()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+
+        public Task Handle(DomainNotification notification, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
