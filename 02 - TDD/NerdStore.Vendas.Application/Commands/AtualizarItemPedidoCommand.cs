1222f454acff90c14318d883716cd071484ad570
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:56:55 -0300

    Create AtualizarItemPedidoCommand.cs

---
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/AtualizarItemPedidoCommand.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class AtualizarItemPedidoCommand : Command
+    {
+
+        public override bool EhValido()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
