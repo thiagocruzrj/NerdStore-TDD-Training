b53da0f2404b13b3fe0e90d0bfaa07eb9288b3d3
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:41:54 -0300

    Create DependencyInjection.cs

---
+++ 03 - Testes de Integracao/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NerdStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+        }
+    }
+}
