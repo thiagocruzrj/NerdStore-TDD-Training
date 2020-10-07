750a1db0fa34eae363bfe0be2ce6e11148b08955
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:58:04 -0300

    Create RemoverItemPedidoCommand.cs

---
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -0,0 +1,12 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class RemoverItemPedidoCommand : Command
+    {
+        public override bool EhValido()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
