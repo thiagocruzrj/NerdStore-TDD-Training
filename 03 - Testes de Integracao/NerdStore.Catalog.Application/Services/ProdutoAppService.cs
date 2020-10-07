91e1c49c17c0bf25311d0f6b0751ca569b144f05
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:06:13 -0300

    Add  di referencves

--- 03 - Testes de Integracao/NerdStore.Catalog.Application/Services/ProdutoAppService.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Application/Services/ProdutoAppService.cs
@@ -1,4 +1,6 @@
-﻿using NerdStore.Catalog.Application.ViewModels;
+﻿using AutoMapper;
+using NerdStore.Catalog.Application.ViewModels;
+using NerdStore.Catalog.Domain;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,6 +9,19 @@ namespace NerdStore.Catalog.Application.Services
 {
     public class ProdutoAppService : IProdutoAppService
     {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IEstoqueService _estoqueService;
+        private readonly IMapper _mapper;
+
+        public ProdutoAppService(IProdutoRepository produtoRepository,
+                                 IMapper mapper,
+                                 IEstoqueService estoqueService)
+        {
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
+            _estoqueService = estoqueService;
+        }
+
         public Task AdicionarProduto(ProdutoViewModel produtoViewModel)
         {
             throw new NotImplementedException();
