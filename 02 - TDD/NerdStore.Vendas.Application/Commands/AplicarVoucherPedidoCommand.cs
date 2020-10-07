865fb8e9e1d7b0cccf2eb5399ffd2e2ecf44f1e8
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:55:16 -0300

    Update AplicarVoucherPedidoCommand.cs

--- 02 - TDD/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
@@ -1,9 +1,19 @@
 ﻿using NerdStore.Core.Messages;
+using System;
 
 namespace NerdStore.Vendas.Application.Commands
 {
     public class AplicarVoucherPedidoCommand : Command
     {
+        public Guid ClienteId { get; private set; }
+        public string CodigoVoucher { get; private set; }
+
+        public AplicarVoucherPedidoCommand(Guid clienteId, string codigoVoucher)
+        {
+            ClienteId = clienteId;
+            CodigoVoucher = codigoVoucher;
+        }
+
         public override bool EhValido()
         {
             throw new System.NotImplementedException();
