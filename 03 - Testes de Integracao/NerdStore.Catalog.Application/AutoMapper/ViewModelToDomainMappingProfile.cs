144cc5c136a54158c65127f289da7863512e2b76
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:03:53 -0300

    Update ViewModelToDomainMappingProfile.cs

--- 03 - Testes de Integracao/NerdStore.Catalog.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -8,6 +8,14 @@ namespace NerdStore.Catalog.Application.AutoMapper
     {
         public ViewModelToDomainMappingProfile()
         {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
         }
     }
 }
