ca2403fae11727e933b63139e52c668020310363
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 22:15:54 -0300

    Update DependencyInjection.cs

--- 03 - Testes de Integracao/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ 03 - Testes de Integracao/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Catalog.Application.Services;
+using NerdStore.Catalog.Data;
+using NerdStore.Catalog.Data.Repository;
+using NerdStore.Catalog.Domain;
+using NerdStore.Core.Messages.CommonMessages;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Data;
 
 namespace NerdStore.WebApp.MVC.Setup
 {
@@ -6,6 +15,23 @@ namespace NerdStore.WebApp.MVC.Setup
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            // Notifications
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+
+
+            // Vendas
+            services.AddScoped<VendasContext>();
+
+            services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AtualizarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<RemoverItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AplicarVoucherPedidoCommand, bool>, PedidoCommandHandler>();
         }
     }
 }
