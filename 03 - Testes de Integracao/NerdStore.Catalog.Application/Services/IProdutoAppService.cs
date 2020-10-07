1a1cda1abfda2fe248aa6fc02309c4d6abb333d7
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:05:12 -0300

    Add interface methods

--- 03 - Testes de Integracao/NerdStore.Catalog.Application/Services/IProdutoAppService.cs
+++ 03 - Testes de Integracao/NerdStore.Catalog.Application/Services/IProdutoAppService.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿using NerdStore.Catalog.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NerdStore.Catalog.Application.Services
 {
     public interface IProdutoAppService : IDisposable
     {
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo);
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<IEnumerable<CategoriaViewModel>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoViewModel produtoViewModel);
+        Task AtualizarProduto(ProdutoViewModel produtoViewModel);
+
+        Task<ProdutoViewModel> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade);
     }
 }
