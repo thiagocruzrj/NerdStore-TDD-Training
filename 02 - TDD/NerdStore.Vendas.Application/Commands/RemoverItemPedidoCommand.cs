b424b0c05167531432fbc0c145595efc09899af5
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:58:27 -0300

    add ctor and properties

--- 02 - TDD/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -1,9 +1,19 @@
 ﻿using NerdStore.Core.Messages;
+using System;
 
 namespace NerdStore.Vendas.Application.Commands
 {
     public class RemoverItemPedidoCommand : Command
     {
+        public Guid ClienteId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+
+        public RemoverItemPedidoCommand(Guid clienteId, Guid produtoId)
+        {
+            ClienteId = clienteId;
+            ProdutoId = produtoId;
+        }
+
         public override bool EhValido()
         {
             throw new System.NotImplementedException();
