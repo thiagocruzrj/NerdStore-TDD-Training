9d62f7f7cfc3892064a3c73548ae8117b036da82
Thiago Cruz <thagocruz@gmail.com>
2020-10-06 21:57:34 -0300

    Add AtualizarItemPedidoValidation

--- 02 - TDD/NerdStore.Vendas.Application/Commands/AtualizarItemPedidoCommand.cs
+++ 02 - TDD/NerdStore.Vendas.Application/Commands/AtualizarItemPedidoCommand.cs
@@ -1,4 +1,5 @@
-﻿using NerdStore.Core.Messages;
+﻿using FluentValidation;
+using NerdStore.Core.Messages;
 using System;
 
 namespace NerdStore.Vendas.Application.Commands
@@ -18,7 +19,30 @@ namespace NerdStore.Vendas.Application.Commands
 
         public override bool EhValido()
         {
-            throw new System.NotImplementedException();
+            ValidationResult = new AtualizarItemPedidoValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class AtualizarItemPedidoValidation : AbstractValidator<AtualizarItemPedidoCommand>
+    {
+        public AtualizarItemPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage("A quantidade miníma de um item é 1");
+
+            RuleFor(c => c.Quantidade)
+                .LessThan(15)
+                .WithMessage("A quantidade máxima de um item é 15");
         }
     }
 }
