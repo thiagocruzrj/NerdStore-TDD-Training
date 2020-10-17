b355b7420db6e7e5dda84d073bbcbcd869676416
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 16:33:53 -0300

    Add entities

--- 02 - TDD/NerdStore.Vendas.Data/VendasContext.cs
+++ 02 - TDD/NerdStore.Vendas.Data/VendasContext.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using NerdStore.Core.Data;
 using NerdStore.Core.Messages;
+using NerdStore.Vendas.Domain;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,8 +18,21 @@ namespace NerdStore.Vendas.Data
             _mediator = mediator;
         }
 
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoItem> PedidoItems { get; set; }
+        public DbSet<Voucher> Vouchers { get; set; }
+
         public async Task<bool> Commit()
         {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+
             var sucesso = await base.SaveChangesAsync() > 0;
             if (sucesso) await _mediator.PublicarEventos(this);
 
