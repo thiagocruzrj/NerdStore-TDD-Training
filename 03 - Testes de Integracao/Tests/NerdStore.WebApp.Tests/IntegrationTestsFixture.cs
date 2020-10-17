8795f70c359e22e7a8be6175e9f839842237ac3d
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 13:45:46 -0300

    Create IntegrationTestsFixture.cs

---
+++ 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class IntegrationTestsFixture<IStartup> : IDisposable where IStartup : class
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
