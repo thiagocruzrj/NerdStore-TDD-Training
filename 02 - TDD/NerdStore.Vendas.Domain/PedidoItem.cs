33ee76dc51263d87ea4c7b3cbb8940065ee5a350
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 16:15:04 -0300

    Update PedidoItem.cs

--- 02 - TDD/NerdStore.Vendas.Domain/PedidoItem.cs
+++ 02 - TDD/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -1,14 +1,22 @@
-﻿using NerdStore.Core.DomainObjects;
-using System;
+﻿using System;
+using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Vendas.Domain
 {
-    public class PedidoItem
+    public class PedidoItem : Entity
     {
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        // EF Rel.
+        public Pedido Pedido { get; set; }
+
         public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
         {
-            if (quantidade < Pedido.MIN_UNIDADES_ITEM)
-                throw new DomainException($"Limite minimo de {Pedido.MIN_UNIDADES_ITEM} itens excedido.");
+            if (quantidade < Pedido.MIN_UNIDADES_ITEM) throw new DomainException($"Mínimo de {Pedido.MIN_UNIDADES_ITEM} unidades por produto");
 
             ProdutoId = produtoId;
             ProdutoNome = produtoNome;
@@ -16,19 +24,21 @@ namespace NerdStore.Vendas.Domain
             ValorUnitario = valorUnitario;
         }
 
-        public Guid ProdutoId { get; private set; }
-        public string ProdutoNome { get; private set; }
-        public int Quantidade { get; private set; }
-        public decimal ValorUnitario { get; private set; }
+        protected PedidoItem() { }
 
-        internal void AdicionarUnidades(int unidades)
+        internal void AssociarPedido(Guid pedidoId)
         {
-            Quantidade += unidades;
+            PedidoId = pedidoId;
         }
 
         internal decimal CalcularValor()
         {
-            return ValorUnitario * Quantidade;
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
         }
 
         internal void AtualizarUnidades(int unidades)
