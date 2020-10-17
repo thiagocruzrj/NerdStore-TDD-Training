1f708c9c4c1b325c22798b9fa011931f3b52ec2b
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 16:30:47 -0300

    Add OnModelCreating override

--- 02 - TDD/NerdStore.Vendas.Data/VendasContext.cs
+++ 02 - TDD/NerdStore.Vendas.Data/VendasContext.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using NerdStore.Core.Data;
+using NerdStore.Core.Messages;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Data
@@ -20,6 +22,23 @@ namespace NerdStore.Vendas.Data
             if (sucesso) await _mediator.PublicarEventos(this);
 
             return sucesso;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(VendasContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.HasSequence<int>("MinhaSequencia").StartsAt(1000).IncrementsBy(1);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
