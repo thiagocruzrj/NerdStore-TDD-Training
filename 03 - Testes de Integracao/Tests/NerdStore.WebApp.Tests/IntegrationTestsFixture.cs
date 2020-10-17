e49c3160ca2d9d6905ccbdd31c2c69cf76554a7f
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 13:50:27 -0300

    Add collections definitions

--- 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
+++ 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using NerdStore.WebApp.MVC;
+using System;
+using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+
     public class IntegrationTestsFixture<IStartup> : IDisposable where IStartup : class
     {
         public void Dispose()
