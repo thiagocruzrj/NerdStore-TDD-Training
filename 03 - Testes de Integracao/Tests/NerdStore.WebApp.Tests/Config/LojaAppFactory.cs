cbd6650b90870d47984fcde3ef3df9372e08b6af
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 13:45:48 -0300

    Update LojaAppFactory.cs

--- 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
+++ 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/Config/LojaAppFactory.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace NerdStore.WebApp.Tests.Config
 {
     public class LojaAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseStartup<TStartup>();
+            builder.UseEnvironment("Testing");
+        }
     }
 }
