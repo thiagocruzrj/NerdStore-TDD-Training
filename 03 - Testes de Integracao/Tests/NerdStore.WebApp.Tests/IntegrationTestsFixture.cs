c19b1dfe23b0242dbd811f73519d2580fec85661
Thiago Cruz <thagocruz@gmail.com>
2020-10-17 13:54:44 -0300

    Update IntegrationTestsFixture.cs

--- 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
+++ 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/IntegrationTestsFixture.cs
@@ -1,5 +1,7 @@
 ﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
 using System;
+using System.Net.Http;
 using Xunit;
 
 namespace NerdStore.WebApp.Tests
@@ -12,9 +14,19 @@ namespace NerdStore.WebApp.Tests
 
     public class IntegrationTestsFixture<IStartup> : IDisposable where IStartup : class
     {
-        public void Dispose()
+        public readonly LojaAppFactory<IStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
         {
-            throw new NotImplementedException();
+            Factory = new LojaAppFactory<IStartup>();
+            Client = Factory.CreateClient();
+        }
+
+        public void Dispose() 
+        {
+            Client.Dispose();
+            Factory.Dispose();
         }
     }
 }
