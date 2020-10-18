fa7ffbfae926d16f67498d78d015c7832bf71e9a
Thiago Cruz <thagocruz@gmail.com>
2020-10-18 14:18:26 -0300

    Update UsuarioTests.cs

--- 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ 03 - Testes de Integracao/Tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -20,6 +20,8 @@ namespace NerdStore.WebApp.Tests
         public async Task Usuario_RealizarCadastro_DeveExecutarComSucesso()
         {
             // Arrange
+            var initialResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
             // Act 
             // Assert
         }
